daba2375cf32318618e31ee833c15dadb2ec029b
DIPRO2000 <diprochak@gmail.com>
2025-03-09 11:12:17 +0530

    EVENT PART UPLOADED

--- src/Pages/Home/Events.jsx
+++ src/Pages/Home/Events.jsx
@@ -5,12 +5,24 @@ const Events=()=>{
         <>
             <div className="bg-[#6c63ff] text-white px-36">
                 <h1 className="text-4xl py-4">EXPLORE OUR EVENTS</h1>
-                <div>
-                    <div>
+                <div className="flex flex-wrap justify-center max-w-6xl gap-5 w-full px-4">
+                    <div className="p-4 w-full flex flex-col items-center sm:w-1/3 bg-slate-700">
                         <img src="https://images.unsplash.com/photo-1738996674608-3d2d9d8450a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D"/>
-                        <h3>CSI STUDENT CHAPTER</h3>
-                        <p>Onboarding to the activities in the world of Cutting-edge technologies</p>
-                        
+                        <h3 className="my-3 text-center text-3xl font-bold text-white">CSI STUDENT CHAPTER</h3>
+                        <p className="my-3 text-center text-xl font-normal text-white">Onboarding to the activities in the world of Cutting-edge technologies</p>
+                        <div className="py-6 px-6 my-6 inline-block bg-orange-700">REGISTER</div>
+                    </div>
+                    <div className="p-4 w-full flex flex-col items-center sm:w-1/3 bg-slate-700">
+                        <img src="https://images.unsplash.com/photo-1738996674608-3d2d9d8450a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D"/>
+                        <h3 className="my-3 text-center text-3xl font-bold text-white">CSI STUDENT CHAPTER</h3>
+                        <p className="my-3 text-center text-xl font-normal text-white">Onboarding to the activities in the world of Cutting-edge technologies</p>
+                        <div className="py-6 px-6 my-6 inline-block bg-orange-700">REGISTER</div>
+                    </div>
+                    <div className="p-4 w-full flex flex-col items-center sm:w-1/3 bg-slate-700">
+                        <img src="https://images.unsplash.com/photo-1738996674608-3d2d9d8450a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D"/>
+                        <h3 className="my-3 text-center text-3xl font-bold text-white">CSI STUDENT CHAPTER</h3>
+                        <p className="my-3 text-center text-xl font-normal text-white">Onboarding to the activities in the world of Cutting-edge technologies</p>
+                        <div className="py-6 px-6 my-6 inline-block bg-orange-700">REGISTER</div>
                     </div>
                 </div>
             </div>
